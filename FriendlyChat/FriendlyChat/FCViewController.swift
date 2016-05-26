f30f47b8b6792795c7abb5e18c534384c70be803
Managam Silalahi <managam.silalahi@gmail.com>
2016-05-26 14:21:46 +0700

    This step https://codelabs.developers.google.com/codelabs/firebase-ios-swift/#6 success.

--- FriendlyChat/FriendlyChat/FCViewController.swift
+++ FriendlyChat/FriendlyChat/FCViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Photos
+
 import Firebase
 
 class FCViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -106,6 +108,10 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
         }
     }
     
+    @IBAction func didSendMessage(sender: UIButton) {
+        textFieldShouldReturn(textField)
+    }
+    
     
     // Helper Function
     func loadAd() {
@@ -118,6 +124,82 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
     
     func configureStorage() {
         
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        guard let text = textField.text else { return true }
+        
+        let newLength = text.utf16.count + string.utf16.count - range.length
+        
+        return newLength <= self.msgLength.integerValue // Bool
+    }
+    
+    func sendMessage(data: [String: String]){
+        var mdata = data
+        mdata[Constants.MessageFields.name] = AppState.sharedInstance.displayName
+        if let photoUrl = AppState.sharedInstance.photoUrl {
+            mdata[Constants.MessageFields.photoUrl] = photoUrl.absoluteString
+        }
+        
+        // Push data to Firebase Database
+        self.ref.child("messages").childByAutoId().setValue(mdata)
+    }
+    
+    // UITextViewDelegate protocol methods
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        let data = [Constants.MessageFields.text : textField.text! as String]
+        sendMessage(data)
+        return true
+    }
+    
+    
+    // MARK: - Image Picker
+    
+    @IBAction func didTapAddPhoto(sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            picker.sourceType = .Camera
+        } else {
+            picker.sourceType = .PhotoLibrary
+        }
+        
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+        let referenceUrl = info[UIImagePickerControllerReferenceURL] as! NSURL
+        let assets = PHAsset.fetchAssetsWithALAssetURLs([referenceUrl], options: nil)
+        let asset = assets.firstObject
+        
+        asset?.requestContentEditingInputWithOptions(nil, completionHandler: { (contentEditingInput, info) in
+            
+            let imageFile = contentEditingInput?.fullSizeImageURL
+            let filePath = "\(FIRAuth.auth()?.currentUser?.uid)/\(Int(NSDate.timeIntervalSinceReferenceDate() * 1000))\(referenceUrl.lastPathComponent!)"
+            
+        })
+        
+    }
+    
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func showAlert(title: String, message: String) {
+        dispatch_async(dispatch_get_main_queue()) { 
+            
+            let alert = UIAlertController.init(title: title, message: message, preferredStyle: .Alert)
+            let dismissAction = UIAlertAction.init(title: "Dismiss", style: .Destructive, handler: nil)
+            alert.addAction(dismissAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
     }
     
 }
